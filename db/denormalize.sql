c68080b27ab3389f2cc0d36909d6c97d4eda4d92
Daniel Choi <dhchoi@gmail.com>
2011-04-28 12:58:39 -0400

    port denormaliz update to postgres

--- db/denormalize.sql
+++ db/denormalize.sql
@@ -20,3 +20,21 @@ SET t.origin = stFirstStop.stop_name,
     t.ends = stLast.arrival_time;
 
 
+/* postgres version */
+UPDATE trips 
+SET t.origin = stFirstStop.stop_name,
+    t.destination = stLastStop.stop_name,
+    t.starts = stFirst.arrival_time,
+    t.ends = stLast.arrival_time
+FROM trips t
+INNER JOIN (
+    SELECT trip_id, MIN(stop_sequence) minS, MAX(stop_sequence) maxS 
+    FROM stop_times
+    GROUP BY trip_id
+) tg ON t.trip_id = tg.trip_id
+JOIN stop_times stFirst ON tg.trip_id = stFirst.trip_id AND stFirst.stop_sequence = tg.minS
+JOIN stop_times stLast ON tg.trip_id = stLast.trip_id AND stLast.stop_sequence = tg.maxS
+JOIN stops stFirstStop ON stFirst.stop_id = stFirstStop.stop_id
+JOIN stops stLastStop ON stLast.stop_id = stLastStop.stop_id;
+
+
