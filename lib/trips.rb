4943753935fecaf91f8880fb1ffb88bf0506af54
Daniel Choi <dhchoi@gmail.com>
2011-05-26 20:16:43 -0400

    more experimentation

--- lib/trips.rb
+++ lib/trips.rb
@@ -4,6 +4,25 @@ DB = Sequel.connect 'postgres:///mbta'
 query = <<QUERY
 select stops.stop_name, stops.stop_code, st.* from stop_times_today('1', 1) st join stops using(stop_id)
 QUERY
+
+trips = Hash.new {|hash, key| hash[key] = []}
+stops = Hash.new {|hash, key| hash[key] = []}
 DB[query].each do |row|
-  puts row.inspect
+  #puts row.inspect
+  trip_id = row[:trip_id]
+  stop_name = row[:stop_name]
+  stopping = [ row[:arrival_time], row[:trip_id] ]
+  trips[trip_id] << stopping
+  stops[stop_name] << stopping
 end
+puts '-' * 80
+
+trips.each do |k, v|
+  puts k
+  puts v.inspect
+end
+stops.each do |k, v|
+  puts k
+  puts v.inspect
+end
+
