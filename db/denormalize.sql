cfe82b96e4554a354ba3ee4358aa6b84270ff672
Daniel Choi <dhchoi@gmail.com>
2011-05-27 14:46:10 -0400

    more efficient sql update

--- db/denormalize.sql
+++ db/denormalize.sql
@@ -2,14 +2,14 @@
 ALTER table trips ADD COLUMN finished_at varchar(12);
 
 UPDATE trips
-SET finished_at  = stLast.arrival_time
+SET finished_at  = tg.last_arrival_time
  FROM trips t
  INNER JOIN (
-     SELECT trip_id, MAX(stop_sequence) maxS
+     SELECT trip_id, MAX(arrival_time) last_arrival_time
      FROM stop_times
      GROUP BY trip_id
- ) tg ON t.trip_id = tg.trip_id
- JOIN stop_times stLast ON tg.trip_id = stLast.trip_id AND stLast.stop_sequence = tg.maxS;
+ ) tg ON t.trip_id = tg.trip_id;
+
  
 
 
