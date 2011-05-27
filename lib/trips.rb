59f9fe893cfb38740a24bd40c99a3d6cbf5eb0d5
Daniel Choi <dhchoi@gmail.com>
2011-05-26 20:33:23 -0400

    some corrections

--- lib/trips.rb
+++ lib/trips.rb
@@ -1,28 +1,24 @@
 require 'sequel'
 DB = Sequel.connect 'postgres:///mbta'
 # don't put semicolon at the end of sequel query
+require 'yaml'
+route = '1'
 query = <<QUERY
-select stops.stop_name, stops.stop_code, st.* from stop_times_today('1', 1) st join stops using(stop_id)
+select stops.stop_name, stops.stop_code, st.* from stop_times_today('#{route}', 1) st join stops using(stop_id)
 QUERY
 
 trips = Hash.new {|hash, key| hash[key] = []}
-stops = Hash.new {|hash, key| hash[key] = []}
+
 DB[query].each do |row|
-  #puts row.inspect
-  trip_id = row[:trip_id]
-  stop_name = row[:stop_name]
-  stopping = [ row[:arrival_time], row[:trip_id] ]
-  trips[trip_id] << stopping
-  stops[stop_name] << stopping
+  stopping = [ row[:arrival_time], row[:stop_name], row[:stop_sequence] ]
+  trips[row[:trip_id]] << stopping
 end
+
 puts '-' * 80
+
+#puts trips.to_yaml
 
 trips.each do |k, v|
   puts k
-  puts v.inspect
+  puts v.sort_by {|x| x[-1]}.inspect
 end
-stops.each do |k, v|
-  puts k
-  puts v.inspect
-end
-
