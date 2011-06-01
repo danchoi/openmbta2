68c5f399ef5659bbba9bb87c14904d99466fd7f2
Daniel Choi <dhchoi@gmail.com>
2011-06-01 00:33:56 -0400

    turning this into a rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Openmbta2::Application.config.secret_token = '0e35b6b661c98a761b5e18f50729bd974ec85164525d612127c5a6221fa80c756b1af1a6b39c744c380a17f35a8d0b722de8c6161c1da9227faea646c34f8297'
