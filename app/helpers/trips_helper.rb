7f6f7d6ba30364df3109b58e9396a0ac71c67349
Daniel Choi <dhchoi@gmail.com>
2011-06-02 14:44:26 -0400

    finish mobile realtime

--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -1,2 +1,10 @@
 module TripsHelper
+
+  def realtime_arrivals(stop_id)
+    x = @result[:stops][stop_id.to_s]
+    return unless x
+    if x[:next_arrivals][-1][0] =~ /realtime/
+      x[:next_arrivals][0..-2].map {|x| x[0]}.join(", ")
+    end
+  end
 end
