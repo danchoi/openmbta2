3d3970ae827589fb016261fe98d1c492f05bae7a
Daniel Choi <dhchoi@gmail.com>
2011-06-02 08:59:44 -0400

    more accurate notice

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -22,7 +22,7 @@ class TripsController < ApplicationController
              end
       render :json => resp.to_json
     rescue TransitTrips::NoRouteData, OpenMBTA::InvalidDirection
-      resp = {message: {title: 'Alert', body: 'No trips found, You may need to update your bookmark, as the dataset has changed.'}}
+      resp = {message: {title: 'Alert', body: 'You may need to delete all yours old bookmarks and create new ones. The dataset has changed. Soryr for the inconvenience.'}}
       render :json => resp.to_json
     end
   end
