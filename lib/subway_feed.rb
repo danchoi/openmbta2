fff8c412a6b2d711abb40d7f581459c64cbdffa1
Daniel Choi <dhchoi@gmail.com>
2011-06-03 11:57:50 -0400

    notes

--- lib/subway_feed.rb
+++ lib/subway_feed.rb
@@ -2,6 +2,7 @@ require 'database'
 require 'nokogiri'
 require 'open-uri'
 require 'csv'
+require 'timeout'
 
 module SubwayFeed
 
@@ -28,23 +29,27 @@ module SubwayFeed
 
     def get_predictions(line)
       puts "Getting predictions for #{line} at #{Time.now}"
-      handle = open("http://developer.mbta.com/Data/#{line}.txt")
-      line = line.capitalize
-      DB.run("delete from rt_subway_predictions  where line = '#{line}'")
-      headers = %w( line trip_id platform_key information_type arrival_time wait_time revenue route ).map {|x| x.to_sym}
-      i = 0
-      CSV.new(handle, headers: headers).each do |row|
-        data = row.to_hash
-        data = data.inject({}) {|memo, (k, v)|
-          memo[k] = v.is_a?(String) ? v.strip : v
-          memo
-        }
-        data.delete(:wait_time)
-        data.delete(:revenue)
-        DB[:rt_subway_predictions].insert data
-        i += 1
+      Timeout::timeout(5) do 
+        handle = open("http://developer.mbta.com/Data/#{line}.txt")
+        line = line.capitalize
+        DB.run("delete from rt_subway_predictions  where line = '#{line}'")
+        headers = %w( line trip_id platform_key information_type arrival_time wait_time revenue route ).map {|x| x.to_sym}
+        i = 0
+        CSV.new(handle, headers: headers).each do |row|
+          data = row.to_hash
+          data = data.inject({}) {|memo, (k, v)|
+            memo[k] = v.is_a?(String) ? v.strip : v
+            memo
+          }
+          data.delete(:wait_time)
+          data.delete(:revenue)
+          DB[:rt_subway_predictions].insert data
+          i += 1
+        end
+        puts "-- #{i} records created"
       end
-      puts "-- #{i} records created"
+    rescue Timeout::Error
+      puts "Timeout error"
     end
 
     def get_all_predictions
