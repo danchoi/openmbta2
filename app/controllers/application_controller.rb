9ff0a0178dc039e31c084f7a1767764bdf03e426
Daniel Choi <dhchoi@gmail.com>
2011-08-29 14:16:47 -0400

    log user agent

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :log_user_agent
+
   layout 'application'
+
+  def log_user_agent
+    logger.info("USER AGENT: #{request.env["HTTP_USER_AGENT"]}")
+  end
 end
