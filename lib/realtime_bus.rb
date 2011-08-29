001f5b7f54c3d5e5e5fb728f93f9650be645cd54
Daniel Choi <dhchoi@gmail.com>
2011-08-29 08:04:42 -0400

    updates for summer 2011 GTFS data set

--- lib/realtime_bus.rb
+++ lib/realtime_bus.rb
@@ -6,7 +6,7 @@ class RealtimeBus
   #
   def self.available?(route, direction_id)
     dataset = DB["select count(*) from nextbus_predictions inner join routes on (trim(leading '0' from  split_part(routes.route_id, '-', 1)) = nextbus_predictions.routetag)     
-      where coalesce(nullif(routes.route_long_name, ''), nullif(routes.route_short_name, '')) = ? and split_part(dirtag, '_', 3) = ? and arrival_time > now()", route, direction_id.to_s].first
+      where coalesce(nullif(routes.route_long_name, ''), nullif(routes.route_short_name, '')) = ? and split_part(dirtag, '_', 2) = ? and arrival_time > now()", route, direction_id.to_s].first
     dataset[:count] > 0
   end
 
@@ -23,7 +23,7 @@ class RealtimeBus
     DB["select * from nextbus_predictions 
       inner join routes on (trim(leading '0' from  split_part(routes.route_id, '-', 1)) = nextbus_predictions.routetag)     
       where coalesce(nullif(routes.route_long_name, ''), nullif(routes.route_short_name, '')) = ? 
-      and split_part(dirtag, '_', 3) = ? order by arrival_time asc", @route, @direction_id.to_s].each do |x|
+      and split_part(dirtag, '_', 2) = ? order by arrival_time asc", @route, @direction_id.to_s].each do |x|
       item = [x[:arrival_time], x[:vehicle]]
       # because the above SQL query can return dup routes (in service at different times)
       if @stops[x[:stoptag]] &&  @stops[x[:stoptag]].detect {|y| y == item }
