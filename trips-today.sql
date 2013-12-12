3b12133d0619b84854857ae16833302ddf1aaf68
Daniel Choi <dhchoi@gmail.com>
2013-12-11 21:51:42 -0500

    fix trips-today.sql daily table creation script

--- trips-today.sql
+++ trips-today.sql
@@ -1,5 +1,12 @@
 delete from trips_today;
-insert into trips_today select * from trips where service_id in (select active_services(adjusted_date(now())) as service_id);
+insert into trips_today select route_id,
+  service_id, trip_id, trip_headsign, direction_id, block_id, 
+  shape_id,
+  finished_at,
+  route_type,
+  coalesce(nullif(r.route_long_name, ''), nullif(r.route_short_name, ''))
+  from trips inner join routes r using (route_id) where service_id in (select active_services(adjusted_date(now())) as service_id);
+
 delete from route_directions_today;
 insert into route_directions_today 
   select route_type, route_coalesced_name route, direction_id, count(*) as total_trips_today from trips_today  group by route_type, route_coalesced_name, direction_id;
