3227046a5687cce546136504e92ca30fdb33ddf1
Daniel Choi <dhchoi@gmail.com>
2014-08-30 14:35:13 -0400

    switch db; remove links to openmbta.com

--- lib/database.rb
+++ lib/database.rb
@@ -4,7 +4,7 @@ require 'dalli'
 
 #DB = Sequel.connect 'postgres:///mbta', :logger => Logger.new(STDOUT)
 unless defined?(DB)
-  DB = Sequel.connect 'postgres:///mbta'
+  DB = Sequel.connect 'postgres:///mbta2'
 end
 
 MEMCACHED = Dalli::Client.new
