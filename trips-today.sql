7aa1a2491d652b5fdfe8ae21e1971b3f8226a5ea
DHC <danchoi@danielchoi.com>
2018-12-07 13:23:16 -0500

    fix trips today bus route names for ct1

--- trips-today.sql
+++ trips-today.sql
@@ -1,10 +1,11 @@
 delete from trips_today;
-insert into trips_today select route_id,
+insert into trips_today 
+  select route_id,
   service_id, trip_id, trip_headsign, direction_id, block_id, 
   shape_id,
   finished_at,
   route_type,
-  (case when r.route_type = 3 then r.route_id else  coalesce(nullif(r.route_long_name, ''), nullif(r.route_short_name, '')) end) 
+  (case when r.route_type = 3 then coalesce(r.route_short_name, r.route_id) else  coalesce(nullif(r.route_long_name, ''), nullif(r.route_short_name, '')) end) 
   from trips inner join routes r using (route_id) where service_id in (select active_services(adjusted_date(now())) as service_id);
 
 delete from route_directions_today;
