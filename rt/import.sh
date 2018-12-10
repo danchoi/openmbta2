d6046ab7472f3ee77a140e43322d76167f995dd0
DHC <danchoi+git@danielchoi.com>
2018-12-09 20:35:36 -0500

    tweaking rt/test.sql

--- rt/import.sh
+++ rt/import.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 db=${1:-mbta}
+input=${2?}
 
-cat trips.json | jq -rcM '.trip.trip_id as $trip_id | .trip.route_id as $route_id | .stop_time_updates[] | select(.arrival.time != null) | [$route_id, $trip_id,  .stop_sequence, .stop_id, .arrival.time] | @tsv ' |
 
-psql $db -c "COPY gtfsrt FROM STDIN WITH DELIMITER E'\t'"
+cat $input | jq -rcM '.trip.trip_id as $trip_id | .trip.route_id as $route_id | .stop_time_updates[] | select(.arrival.time != null) | [$route_id, $trip_id,  .stop_sequence, .stop_id, .arrival.time] | @tsv ' |
+
+psql $db -c "truncate gtfsrt ; COPY gtfsrt FROM STDIN WITH DELIMITER E'\t'"
 
--- rt/test.sql
+++ rt/test.sql
@@ -2,6 +2,7 @@ select gtfsrt.*,
 -- if trip is ADDED, we must get the direction from the stop
 coalesce(trips.direction_id, 
   (select direction_id from trips inner join stop_times using (trip_id) where stop_id = gtfsrt.stop_id  limit 1)
-)
+) direction_id,
+to_char(to_timestamp(gtfsrt.arrival_time), 'HH24:MI') as fmtime
 from gtfsrt left join trips using (trip_id);  
 
