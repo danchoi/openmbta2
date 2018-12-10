bc7653637504870c5dea3eaeb9832419c3169062
DHC <danchoi+git@danielchoi.com>
2018-12-09 20:28:47 -0500

    reformat

--- rt/test.sql
+++ rt/test.sql
@@ -1,5 +1,7 @@
 select gtfsrt.*, 
 -- if trip is ADDED, we must get the direction from the stop
-coalesce(trips.direction_id, (select direction_id from trips inner join stop_times using (trip_id) where stop_id = gtfsrt.stop_id  limit 1))
+coalesce(trips.direction_id, 
+  (select direction_id from trips inner join stop_times using (trip_id) where stop_id = gtfsrt.stop_id  limit 1)
+)
 from gtfsrt left join trips using (trip_id);  
 
