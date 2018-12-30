1aa2faacb94f53a51e1b025297263f0ae671d1c9
Daniel Choi <dhchoi@gmail.com>
2018-12-30 09:26:59 -0500

    add check.sql

---
+++ check.sql
@@ -0,0 +1,6 @@
+select count(*) from routes;
+select count(*) from trips;
+select count(*) from stops;
+select count(*) from stop_times;
+select count(*) from trips_today;
+select max(end_date) from calendar;
--- db/gen_load1.rb
+++ db/gen_load1.rb
@@ -2,9 +2,8 @@ require 'csv'
 DB=ARGV[0] || abort("DB name as 1st arg")
 Dir["data/*.txt"].
   select {|f| 
-    f !~ /calendar|feed_info|facilities|levels|pathways|transfers|checkpoints|lines|multi_route_trips|directions/
+    ["data/agency.txt", "data/trips.txt", "data/stop_times.txt" ].include?(f)
   }.each do |file|
-
   $stderr.print "Processing file: #{file} -> "
   table = File.basename(file, ".txt")
   head = `head -1 #{file}`.split("\r\n")[0]
@@ -25,4 +24,11 @@ SQL
   puts sql
 end
 
-
+puts <<END
+echo 'loading table stops'
+psql #{DB} -c "truncate stops;"
+cat data/stops.txt | psql #{DB} -c "copy stops (stop_id,stop_name,stop_desc,stop_lat,stop_lon,level_id,location_type,parent_station,wheelchair_boarding,stop_code,platform_code,platform_name,zone_id,stop_url,stop_address) from STDIN WITH DELIMITER ','  CSV HEADER;"
+echo 'loading table routes'
+psql #{DB} -c "truncate routes;"
+cat data/routes.txt | psql #{DB} -c "copy routes ( route_id,agency_id,route_short_name,route_long_name,route_desc,route_fare_class,route_type,route_url,route_color,route_text_color,route_sort_order,line_id,listed_route) from STDIN WITH DELIMITER ','  CSV HEADER;"
+END
